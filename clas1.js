cd90d1ec081aed34cff7055c15e5e86bc2b7c30c
Fahrurroziy <148849905+fahrurroziy80@users.noreply.github.com>
2025-02-06 16:22:43 +0700

    clas1

---
+++ clas1.js
@@ -0,0 +1,18 @@
+// var nama = "fahrurrozi";
+// let umur = 20;
+// const tinggi = 170;
+
+// console.log("saya bernama", nama);
+// console.log("umur saya adalah", umur);
+// console.log("tinggi saya adalah", tinggi);
+
+
+let nama = "Budi"; // String
+let umur = 30; // Number
+let isStudent = true; // Boolean
+let hobi = ["Membaca", "Gaming", "Olahraga"]; // Array
+let dataDiri = { nama: "Budi", umur: 30, status: "Mahasiswa" }; // Object
+let kosong = null; // Null
+let belumDidefinisikan; // Undefined
+
+console.log(nama, umur, isStudent, hobi, dataDiri, kosong, belumDidefinisikan);
