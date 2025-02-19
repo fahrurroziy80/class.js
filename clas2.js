9602177073281573a44a3a15aba5ed5a056cf4ec
Fahrurroziy <148849905+fahrurroziy80@users.noreply.github.com>
2025-02-19 09:53:31 +0700

    Add files via upload

---
+++ clas2.js
@@ -0,0 +1,12 @@
+//perulangan
+// for (i = 0; i < 10 ; i ++) {
+//     console.log("hallo Word")
+// }
+
+
+//fungsi
+// function prinHallo() {
+//     console.log ("hallo")
+// }
+// prinHallo();
+// prinHallo();
